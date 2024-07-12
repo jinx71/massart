439b9c93e70ebcf87fd576a5db48cbd6d2214f94
jinx71 <karim.sazed@gmail.com>
2024-07-12 22:47:15 +0600

    Bottom Logo Updated

--- src/components/SocialContributor/SocialContributer.jsx
+++ src/components/SocialContributor/SocialContributer.jsx
@@ -2,69 +2,69 @@ import Image from "next/image";
 
 const SocialContributor = () => {
   return (
-    <div className="px-[32px] py-[60px]">
+    <div className="px-[32px] py-[30px]">
       <div className="w-full grid grid-cols-3 gap-4 justify-between items-center">
-        <div className="w-full flex justify-center items-center h-[86px] hover:bg-EventPartner">
+        <div className="w-full h-full lg:p-6 flex justify-center items-center  hover:bg-EventPartner">
           <Image
-            className="w-[90px] h-[25px] object-cover"
+            className="object-cover"
             src="/images/BritishCouncil_Logo.png"
             alt="logo"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
           />
         </div>
 
-        <div className="w-full flex justify-center items-center h-[86px] hover:bg-EventPartner">
+        <div className="w-full h-full lg:p-6 flex justify-center items-center  hover:bg-EventPartner">
           <Image
-            className="w-[75px] h-[61px] object-cover"
+            className="object-cover"
             src="/images/unesco.png"
             alt="logo"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
           />
         </div>
 
-        <div className="w-full flex justify-center items-center h-[86px] hover:bg-EventPartner">
+        <div className="w-full h-full lg:p-6 flex justify-center items-center hover:bg-EventPartner">
           <Image
-            className="w-[73px] h-[40px] object-cover"
+            className=" object-cover"
             src="/images/india.png"
             alt="logo"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
           />
         </div>
       </div>
 
       <hr className="bg-black my-[10px]" />
 
-      <div className="w-full grid grid-cols-3 gap-4 justify-between items-center">
-        <div className="w-full flex justify-center items-center h-[86px] hover:bg-EventPartner">
+      <div className="grid grid-cols-3 gap-3 justify-between items-center">
+        <div className="w-full h-full lg:p-6 flex justify-center items-center hover:bg-EventPartner">
           <Image
-            className="w-[90px] h-[25px] object-cover"
+            className="object-cover"
             src="/images/BritishCouncil_Logo.png"
             alt="logo"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
           />
         </div>
 
-        <div className="w-full flex justify-center items-center h-[86px] hover:bg-EventPartner">
+        <div className="w-full h-full lg:p-6 flex justify-center items-center  hover:bg-EventPartner">
           <Image
-            className="w-[75px] h-[61px] object-cover"
+            className="object-cover"
             src="/images/unesco.png"
             alt="logo"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
           />
         </div>
 
-        <div className="w-full flex justify-center items-center h-[86px] hover:bg-EventPartner">
+        <div className="w-full h-full lg:p-6 flex justify-center items-center  hover:bg-EventPartner">
           <Image
-            className="w-[73px] h-[40px] object-cover"
+            className=" object-cover"
             src="/images/india.png"
             alt="logo"
-            width={100}
-            height={100}
+            width={300}
+            height={300}
           />
         </div>
       </div>
